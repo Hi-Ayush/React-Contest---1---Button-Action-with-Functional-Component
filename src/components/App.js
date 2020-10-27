bd0d9cb098e986ec2315693eb211cff2f37b710f
Ayush Kumar <68886542+Hi-Ayush@users.noreply.github.com>
2020-10-27 21:17:29 +0530

    changes

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,25 @@
-import React, {Component, useState} from "react";
+import React, { Component, useState } from "react";
 import "./../styles/App.css";
 
 function App() {
+  const [isRenderPara, setIsRenderPara] = React.useState(false);
+  function handleClick() {
+    setIsRenderPara(true);
+  }
+
   return (
     <div id="main">
-      // Do not alter the main div
+      <button id="click" onClick={handleClick}>
+        Click Me
+      </button>
+      {isRenderPara && (
+        <p id="para">
+          Hello, I've learnt to use the full-stack evaluation tool. This makes
+          me so happy
+        </p>
+      )}
     </div>
   );
 }
-
 
 export default App;
